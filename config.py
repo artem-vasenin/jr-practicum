ebd65988c87e2481af924144cf3532bc8a3ee1f9
Rusich <vasenin@verqor.com>
2025-02-22 11:22:00 +0300

    added config write

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 GPT_TOKEN = os.getenv("GPT_TOKEN")
 GPT_TOKEN_TWO = os.getenv("GPT_TOKEN_TWO")
+GPT_TOKEN_THREE = os.getenv("GPT_TOKEN_THREE")
 TG_TOKEN = os.getenv("TG_TOKEN")
 PROXY = os.getenv("PROXY")
+PROXY_THREE = os.getenv("PROXY_THREE")
