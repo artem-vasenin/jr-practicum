e889f247b801507af5e92d679ccb01434acb4978
Rusich <vasenin@verqor.com>
2025-02-23 10:25:58 +0300

    added images

--- handlers/ai_handlers.py
+++ handlers/ai_handlers.py
@@ -16,14 +16,14 @@ ai_client = AsyncOpenAI(
 AI_MODEL = 'gpt-3.5-turbo' # 'gpt-4o' дорогой гад
 
 async def get_prompt(path: str):
-    async with aiofiles.open(path, 'r', encoding='utf-8') as f:
+    async with aiofiles.open(f'resources/prompts/{path}.txt'  , 'r', encoding='utf-8') as f:
         return await f.read()
 
 @ai_router.message(F.text == 'Рандомный факт')
 @ai_router.message(F.text == 'Хочу еще факт!')
 @ai_router.message(Command('random'))
 async def ai_random(message: Message):
-    prompt = await get_prompt('resources/prompts/random.txt')
+    prompt = await get_prompt('random')
     await message.bot.send_chat_action(message.from_user.id, ChatAction.TYPING)
     cmp = await ai_client.chat.completions.create(
         messages=[{'role': 'system', 'content': prompt}],
--- handlers/com_handlers.py
+++ handlers/com_handlers.py
@@ -6,6 +6,10 @@ from keyboards import kb_start
 
 command_router = Router()
 
+@command_router.message(F.photo)
+async def get_photo(message: Message):
+    ...
+
 @command_router.message(F.text == 'Назад')
 @command_router.message(Command('start'))
 async def com_start(message: Message):
